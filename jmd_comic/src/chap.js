25bf6ea327332108ffb989d15eb835ca4c502aa3
doanthien123z <takiyasha123z@gmail.com>
2025-12-12 21:15:28 +0700

    Enhance image processing in JMD comic plugin to support 3D images and filter out unwanted formats

--- jmd_comic/src/chap.js
+++ jmd_comic/src/chap.js
@@ -6,24 +6,40 @@ function execute(url) {
     let response = fetch(url);
     if (response.ok) {
         let doc = response.html();
-        
-        let el = doc.select(".content img[data-original]");
-        
         let data = [];
+        
+        let title = doc.select("h1").text();
+        let is3D = title.includes("[3D]");
+
+        let el = doc.select(".content div img.lazy");
+        if (el.size() === 0) {
+            el = doc.select(".content img[data-original]");
+        }
+
         for (let i = 0; i < el.size(); i++) {
             let e = el.get(i);
-            let imgUrl = e.attr("data-original");
             
+            let imgUrl = e.attr("data-original");
             if (!imgUrl) imgUrl = e.attr("src");
             
             if (imgUrl) {
                 if (imgUrl.startsWith("//")) {
                     imgUrl = "https:" + imgUrl;
                 }
-                data.push(imgUrl);
+                
+                if (imgUrl.match(/\.(jpg|jpeg|png|webp)/i) && 
+                    !imgUrl.includes("logo") && 
+                    !imgUrl.includes("icon")) {
+                    
+                    if (is3D) {
+                        let workerUrl = "https://rotate-image.rotate-image.workers.dev/proxy?url=" + imgUrl;
+                        data.push(workerUrl);
+                    } else {
+                        data.push(imgUrl);
+                    }
+                }
             }
         }
-        
         return Response.success(data);
     }
     return null;
